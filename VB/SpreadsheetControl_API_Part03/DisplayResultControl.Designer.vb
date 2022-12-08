1912feeb8da1323d65f0bbabc83ce8563467172c
DevExpressExampleBot <fakeEmail>
2022-12-08 14:52:27 +0400

    autogenerate VB

--- VB/SpreadsheetControl_API_Part03/DisplayResultControl.Designer.vb
+++ VB/SpreadsheetControl_API_Part03/DisplayResultControl.Designer.vb
@@ -1,5 +1,7 @@
-﻿Namespace SpreadsheetControl_API_Part03
-    Partial Public Class DisplayResultControl
+Namespace SpreadsheetControl_API_Part03
+
+    Partial Class DisplayResultControl
+
         ''' <summary> 
         ''' Required designer variable.
         ''' </summary>
@@ -10,14 +12,14 @@
         ''' </summary>
         ''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         Protected Overrides Sub Dispose(ByVal disposing As Boolean)
-            If disposing AndAlso (components IsNot Nothing) Then
-                components.Dispose()
+            If disposing AndAlso (Me.components IsNot Nothing) Then
+                Me.components.Dispose()
             End If
+
             MyBase.Dispose(disposing)
         End Sub
 
-        #Region "Component Designer generated code"
-
+'#Region "Component Designer generated code"
         ''' <summary> 
         ''' Required method for Designer support - do not modify 
         ''' the contents of this method with the code editor.
@@ -31,16 +33,16 @@
             Me.spreadsheetNameBoxControl1 = New DevExpress.XtraSpreadsheet.SpreadsheetNameBoxControl()
             Me.splitContainerControl1 = New DevExpress.XtraEditors.SplitContainerControl()
             Me.splitterControl1 = New DevExpress.XtraEditors.SplitterControl()
-            CType(Me.dockManager1, System.ComponentModel.ISupportInitialize).BeginInit()
-            CType(Me.spreadsheetNameBoxControl1.Properties, System.ComponentModel.ISupportInitialize).BeginInit()
-            CType(Me.splitContainerControl1, System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((Me.dockManager1), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((Me.spreadsheetNameBoxControl1.Properties), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((Me.splitContainerControl1), System.ComponentModel.ISupportInitialize).BeginInit()
             Me.splitContainerControl1.SuspendLayout()
             Me.SuspendLayout()
             ' 
             ' dockManager1
             ' 
             Me.dockManager1.Form = Me
-            Me.dockManager1.TopZIndexControls.AddRange(New String() { "DevExpress.XtraBars.BarDockControl", "DevExpress.XtraBars.StandaloneBarDockControl", "System.Windows.Forms.StatusBar", "System.Windows.Forms.MenuStrip", "System.Windows.Forms.StatusStrip", "DevExpress.XtraBars.Ribbon.RibbonStatusBar", "DevExpress.XtraBars.Ribbon.RibbonControl", "DevExpress.XtraBars.Navigation.OfficeNavigationBar", "DevExpress.XtraBars.Navigation.TileNavPane"})
+            Me.dockManager1.TopZIndexControls.AddRange(New String() {"DevExpress.XtraBars.BarDockControl", "DevExpress.XtraBars.StandaloneBarDockControl", "System.Windows.Forms.StatusBar", "System.Windows.Forms.MenuStrip", "System.Windows.Forms.StatusStrip", "DevExpress.XtraBars.Ribbon.RibbonStatusBar", "DevExpress.XtraBars.Ribbon.RibbonControl", "DevExpress.XtraBars.Navigation.OfficeNavigationBar", "DevExpress.XtraBars.Navigation.TileNavPane"})
             ' 
             ' hideContainerLeft
             ' 
@@ -76,7 +78,7 @@
             Me.spreadsheetNameBoxControl1.Location = New System.Drawing.Point(0, 0)
             Me.spreadsheetNameBoxControl1.MinimumSize = New System.Drawing.Size(0, 20)
             Me.spreadsheetNameBoxControl1.Name = "spreadsheetNameBoxControl1"
-            Me.spreadsheetNameBoxControl1.Properties.Buttons.AddRange(New DevExpress.XtraEditors.Controls.EditorButton() { New DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)})
+            Me.spreadsheetNameBoxControl1.Properties.Buttons.AddRange(New DevExpress.XtraEditors.Controls.EditorButton() {New DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)})
             Me.spreadsheetNameBoxControl1.Size = New System.Drawing.Size(145, 20)
             Me.spreadsheetNameBoxControl1.SpreadsheetControl = Me.spreadsheetControl1
             Me.spreadsheetNameBoxControl1.TabIndex = 0
@@ -112,22 +114,26 @@
             Me.Controls.Add(Me.splitContainerControl1)
             Me.Name = "DisplayResultControl"
             Me.Size = New System.Drawing.Size(800, 600)
-            CType(Me.dockManager1, System.ComponentModel.ISupportInitialize).EndInit()
-            CType(Me.spreadsheetNameBoxControl1.Properties, System.ComponentModel.ISupportInitialize).EndInit()
-            CType(Me.splitContainerControl1, System.ComponentModel.ISupportInitialize).EndInit()
+            CType((Me.dockManager1), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((Me.spreadsheetNameBoxControl1.Properties), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((Me.splitContainerControl1), System.ComponentModel.ISupportInitialize).EndInit()
             Me.splitContainerControl1.ResumeLayout(False)
             Me.ResumeLayout(False)
-
         End Sub
 
-        #End Region
-
+'#End Region
         Private dockManager1 As DevExpress.XtraBars.Docking.DockManager
+
         Private hideContainerLeft As DevExpress.XtraBars.Docking.AutoHideContainer
+
         Private spreadsheetControl1 As DevExpress.XtraSpreadsheet.SpreadsheetControl
+
         Private splitterControl1 As DevExpress.XtraEditors.SplitterControl
+
         Private splitContainerControl1 As DevExpress.XtraEditors.SplitContainerControl
+
         Private spreadsheetNameBoxControl1 As DevExpress.XtraSpreadsheet.SpreadsheetNameBoxControl
+
         Private spreadsheetFormulaBarControl1 As DevExpress.XtraSpreadsheet.SpreadsheetFormulaBarControl
     End Class
 End Namespace
--- VB/SpreadsheetControl_API_Part03/DisplayResultControl.vb
+++ VB/SpreadsheetControl_API_Part03/DisplayResultControl.vb
@@ -1,27 +1,21 @@
-﻿Imports System
-Imports System.Collections.Generic
 Imports System.ComponentModel
 Imports System.Drawing
-Imports System.Data
-Imports System.Linq
-Imports System.Text
-Imports System.Threading.Tasks
 Imports System.Windows.Forms
 Imports DevExpress.XtraSpreadsheet
 
 Namespace SpreadsheetControl_API_Part03
-    Partial Public Class DisplayResultControl
+
+    Public Partial Class DisplayResultControl
         Inherits UserControl
 
         Public Sub New()
             InitializeComponent()
         End Sub
 
-        Public ReadOnly Property Speadsheet() As SpreadsheetControl
+        Public ReadOnly Property Speadsheet As SpreadsheetControl
             Get
                 Return spreadsheetControl1
             End Get
         End Property
-
     End Class
 End Namespace
